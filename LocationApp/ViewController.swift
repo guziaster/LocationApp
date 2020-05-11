5a5d0e1b90e109496cc43da1f52d07a9d547a895
Piotr Guzia <peter.guzia@gmail.com>
2020-05-11 22:17:03 +0200

    Project init

---
+++ LocationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationApp
+//
+//  Created by Piotr Guzia on 11/05/2020.
+//  Copyright © 2020 Piotr Guzia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
